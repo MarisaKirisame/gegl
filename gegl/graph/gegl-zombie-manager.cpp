0426fd2cd56785f84cba034bb5ecb6e9ca7ec62b
Marisa Kirisame <marisa@cs.utah.edu>
2023-04-17 00:22:06 -0600

    save

--- gegl/graph/gegl-zombie-manager.cpp
+++ gegl/graph/gegl-zombie-manager.cpp
@@ -180,7 +180,7 @@ struct _GeglZombieManager {
     // todo: calculate parent dependency
     auto tile_size = GetTileSize();
     if (node->cache != nullptr) {
-      ZombieTile zt(bindZombie([tile_size](){ return ZombieTile(tile_size); }));
+      ZombieTile zt(bindZombie([tile_size](){ return ZombieTile(Proxy(tile_size); }));
       zt.evict(); // doing a single eviction to make sure we can recompute
       return zt;
     } else {
