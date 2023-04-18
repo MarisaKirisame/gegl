3ee0bac03c4b61efbc291dfcd9b256668bf17058
Marisa Kirisame <marisa@cs.utah.edu>
2023-04-17 18:17:26 -0600

    save

--- gegl/gegl-config.c
+++ gegl/gegl-config.c
@@ -170,6 +170,7 @@ gegl_config_set_property (GObject      *gobject,
         config->swap_compression = g_value_dup_string (value);
         break;
       case PROP_THREADS:
+        raise(SIGINT);
         _gegl_threads = g_value_get_int (value);
         return;
       case PROP_USE_OPENCL:
