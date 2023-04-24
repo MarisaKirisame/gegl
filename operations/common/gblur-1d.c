fcab7918d423349465fdbf44fbd045e78dd1d51f
Marisa Kirisame <marisa@cs.utah.edu>
2023-04-24 04:45:54 -0600

    save

--- operations/common/gblur-1d.c
+++ operations/common/gblur-1d.c
@@ -635,7 +635,7 @@ iir_young_hor_blur (IirYoungBlur1dFunc   real_blur_1D,
       get_boundaries (policy, row, rect->width, nc, &iminus, &uplus);
       real_blur_1D (row, tmp, b, m, iminus, uplus, rect->width, nc, policy);
 
-      gegl_buffer_set (dst, GEGL_RECTANGLE(cur_row->x + 3, cur_row->y, cur_row->width - 6, cur_row->height), level, format, &row[(3 + 3) * nc],
+      gegl_buffer_set (dst, GEGL_RECTANGLE(cur_row.x + 3, cur_row.y, cur_row.width - 6, cur_row.height), level, format, &row[(3 + 3) * nc],
                        GEGL_AUTO_ROWSTRIDE);
     }
 
