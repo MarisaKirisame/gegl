864b85083092152343f1ad2068839648001bf3b4
Marisa Kirisame <marisa@cs.utah.edu>
2023-04-25 23:56:34 -0600

    save

--- gegl/graph/gegl-zombie-manager.cpp
+++ gegl/graph/gegl-zombie-manager.cpp
@@ -149,6 +149,7 @@ struct NodePropertyTable {
     incremental_["gimp:replace"] = true;
     incremental_["gimp:mask-components"] = true;
     incremental_["gegl:write-buffer"] = true;
+    incremental_["gegl:invert-gamma"] = true;
   }
   static const NodePropertyTable& GetNodePropertyTable() {
     static NodePropertyTable npt;
