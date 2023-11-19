a0b6dbeaa10ec8b1d8e61e797a1c89fc587c7a37
ç® <2447746158@qq.com>
2023-11-19 23:10:18 +0800

    debug

--- gegl/graph/gegl-zombie-manager.cpp
+++ gegl/graph/gegl-zombie-manager.cpp
@@ -101,10 +101,12 @@ struct ProxyInside {
     GeglBuffer* buffer_ptr = (GeglBuffer*)g_weak_ref_get(&buffer_ref);
 
     if (buffer_ptr != NULL) {
+      fclose(file);
       if (!(std::get<0>(key) == 0 && std::get<1>(key) == 0)) {
         gegl_buffer_force_clear_tile(buffer_ptr, std::get<0>(key), std::get<1>(key), NULL);
       }
       g_object_unref(buffer_ptr);
+      file = fopen("proxy.log", "a");
     } else {
       fprintf(file, "buffer_ptr is null\n");
     }
